1805d5eae279e8c154ac4c066c994a6c7ea32f1e
MiukkiK <113989655+MiukkiK@users.noreply.github.com>
2024-05-09 19:48:04 +0300

    Initial commit

---
+++ ostoslista.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Ostoslista</title>
+</head>
+<body align="center">
+<?php
+echo "<h1>Hello World!</h1>";
+?>
+	<p id="data">empty</p>
+	<select name="tuotteet" id="tuotelista">
+		<option value="test">testiarvo</option>
+		<option value="test2">testiarvo2</option>
+	</select>
+
+	<p id="valitut">
+		Tuote 1
+		<button id="remove1">X</button>
+	</p>
+</body>
+</html>
