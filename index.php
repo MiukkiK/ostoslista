13cb79467836a53d330a701e31dd778ae4a0f648
MiukkiK <113989655+MiukkiK@users.noreply.github.com>
2024-11-19 01:37:45 +0200

    First draft
    Added mock functionality of shopping list for testing, and txt files for data storage.

--- index.php
+++ index.php
@@ -5,17 +5,38 @@
 </head>
 <body align="center">
 <?php
-echo "<h1>Hello World!</h1>";
+	echo "<h1>Hello World!</h1>";
+	if (array_key_exists("tuotteet", $_POST)) {
+		echo $_POST["tuotteet"];
+		$selectfile = fopen("data/selected.txt", "a") or die("Write error");
+		fwrite($selectfile, $_POST["tuotteet"]);
+		fclose($selectfile);
+	}
+
+	$datafile = fopen("data/data.txt","r") or die("Read error");
+	$data = [];
+	while(!feof($datafile)) {
+  		$data[] = fgets($datafile);
+	}
+	fclose($datafile);
 ?>
-	<p id="data">empty</p>
-	<select name="tuotteet" id="tuotelista">
-		<option value="test">testiarvo</option>
-		<option value="test2">testiarvo2</option>
-	</select>
+	<form action="ostoslista" method="post">
+		<select name="tuotteet" id="tuotelista">
+		<option value="blank">--Valitse tuote--</option>
+		<?php
+			$i = 0;
+			foreach ($data as $x) {
+				echo '<option "value=data' . $i . '">' . $x . '</option>';
+				$i++;
+			}
+		?>
+		</select>
+		<input type=submit value="Add">
+	</form>
 
 	<p id="valitut">
 		Tuote 1
-		<button id="remove1">X</button>
+		<button id="remove1">Remove</button>
 	</p>
 </body>
 </html>
